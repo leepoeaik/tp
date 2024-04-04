37f98535c5c9ae4dbbbfd3bd62d3858cab586b6b
leepoeaik <poeaik.lee@gmail.com>
2024-04-04 15:51:40 +0800

    Enlarge label

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.input.KeyCombination;
@@ -39,6 +40,8 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane commandBoxPlaceholder;
+    @FXML
+    private Label upcomingLessonLabel;
 
     @FXML
     private MenuItem helpMenuItem;
